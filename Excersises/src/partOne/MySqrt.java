61d2f0f0e3886b90c42e0da0f324929b97577c62
ErikKarlkvist <erikkarlkvist95@gmail.com>
2016-01-25 10:45:09 +0100

    Added first package

---
+++ Excersises/src/partOne/MySqrt.java
@@ -0,0 +1,11 @@
+package partOne;
+
+public class MySqrt {
+	public static double mySqrtLoop(double x, double epsilon){
+		return 0.0;
+	}
+	public static double mySqrtRecurse(double x, double epsilon){
+		return 0.0;
+	}
+	public static void main(String args[]){}
+}
