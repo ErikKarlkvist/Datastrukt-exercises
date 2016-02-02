565ad4ff57530534fad09f8437f3d691bc29361d
ErikKarlkvist <erikkarlkvist95@gmail.com>
2016-02-02 11:34:24 +0100

    IT'S WORKING!!!

--- Excersises/src/partTwo/DLList.java
+++ Excersises/src/partTwo/DLList.java
@@ -3,88 +3,96 @@ package partTwo;
 /** Doubly-linked list with user access to nodes
  */
 public class DLList<E> {
-  public class Node {
-    /** The contents of the node is public */
-    public E elt;
+	public class Node {
+		/** The contents of the node is public */
+		public E elt;
 
-    protected Node prev, next;
+		protected Node prev, next;
 
-    Node() {
-      this(null);
-    }
-    Node(E elt) {
-      this.elt = elt;
-      prev = next = null;
-    }
+		Node() {
+			this(null);
+		}
+		Node(E elt) {
+			this.elt = elt;
+			prev = next = null;
+		}
 
-    public Node getNext() {
-      // TODO
-    }
+		public Node getNext() {
+			return next;
+		}
 
-    public Node getPrev() {
-      // TODO
-    }
-  }
-  
-  /** first and last nodes in list, null when list is empty */
-  Node first, last;
-  
-  DLList() {
-    first = last = null;
-  }
-  
-  /** inserts an element at the beginning of the list
-   * @param e   the new element value
-   * @return    the node holding the added element
-   */
-  public Node addFirst(E e) {
-      // TODO
-  }
+		public Node getPrev() {
+			return prev;
+		}
+	}
 
-  /** inserts an element at then end of the list
-   * @param e   the new element
-   * @return    the node holding the added element
-   */
-  public Node addLast(E e) {
-      // TODO
-  }
-  
-  /**
-   * @return    the node of the list's first element, null if list is empty
-   */
-  public Node getFirst() {
-      // TODO
-  }
-  
-  /**
-   * @return    the node of the list's last element, null if list is empty
-   */
-  public Node getLast() {
-      // TODO
-  }
-  
-  /** inserts a new element after a specified node
-    * @param e   the new element
-    * @param l   the node after which to insert the element, must be non-null and a node belonging to this list
-    * @return    the node holding the inserted element
-    */
-  public Node insertAfter(E e, Node l) {
-      // TODO
-  }
+	/** first and last nodes in list, null when list is empty */
+	Node first, last;
 
-  /** inserts a new element before a specified node
-    * @param e   the new element
-    * @param l   the node before which to insert the element, must be non-null and a node belonging to this list
-    * @return    the node holding the inserted element
-    */
-  public Node insertBefore(E e, Node l) {
-      // TODO
-  }
+	DLList() {
+		first = last = null;
+	}
 
-  /** removes an element
-    * @param l   then node containing the element that will be removed, must be non-null and a node belonging to this list
-    */
-  public void remove(Node l) {
-      // TODO
-  }
+	/** inserts an element at the beginning of the list
+	 * @param e   the new element value
+	 * @return    the node holding the added element
+	 */
+	public Node addFirst(E e) {
+		Node newNode = new Node(e);
+		if(first == null){
+			last = newNode;
+		}else{
+			first.prev = newNode;
+		}
+		newNode.next = first;
+		first = newNode;
+		return newNode;
+	}
+
+	/** inserts an element at then end of the list
+	 * @param e   the new element
+	 * @return    the node holding the added element
+	 */
+	public Node addLast(E e) {
+		// TODO
+	}
+
+	/**
+	 * @return    the node of the list's first element, null if list is empty
+	 */
+	public Node getFirst() {
+		// TODO
+	}
+
+	/**
+	 * @return    the node of the list's last element, null if list is empty
+	 */
+	public Node getLast() {
+		// TODO
+	}
+
+	/** inserts a new element after a specified node
+	 * @param e   the new element
+	 * @param l   the node after which to insert the element, must be non-null and a node belonging to this list
+	 * @return    the node holding the inserted element
+	 */
+	public Node insertAfter(E e, Node l) {
+		// TODO
+	}
+
+	/** inserts a new element before a specified node
+	 * @param e   the new element
+	 * @param l   the node before which to insert the element, must be non-null and a node belonging to this list
+	 * @return    the node holding the inserted element
+	 */
+	public Node insertBefore(E e, Node l) {
+		// TODO
+	}
+
+	/** removes an element
+	 * @param l   then node containing the element that will be removed, must be non-null and a node belonging to this list
+	 */
+	public void remove(Node l) {
+		// TODO
+	}
 }
--- Excersises/src/partTwo/Lab2a.java
+++ Excersises/src/partTwo/Lab2a.java
@@ -1,69 +1,93 @@
 package partTwo;
 
+import java.awt.Color;
+
 public class Lab2a {
-  public static double[] simplifyShape(double[] poly, int k)
-  {
-	  double[] polyCopy = poly.clone();
-	  int pLength = polyCopy.length;
-	  double minValue = Double.MAX_VALUE;
-	  int indexSValue = -1;
-	  if(pLength>4){
-		  while(pLength/2 > k){
-			for(int i = 0; i < pLength-6; i+=2){
-				double L = calcDist(polyCopy[i],polyCopy[i+1]);
-				double P = calcDist(polyCopy[i+2],polyCopy[i+3]);
-				double R = calcDist(polyCopy[i+4],polyCopy[i+5]);
-				
-				double l1 = L - P;
-				double l2 = P - R;
-				double l3 = L - R;
-				double value = (l1 + l2 - l3);
-				/*if(value==0){
-					System.out.println("if(value == 0)"+value);
-					polyCopy = removePoint(polyCopy, i);
-					pLength = polyCopy.length;
-				} else*/ 
-				if(value < minValue){
-					minValue = value;
-					indexSValue = i+2;
-				}
-			}//end for
-			System.out.println("End of for" + minValue);
-			polyCopy = removePoint(polyCopy, indexSValue);
-			pLength = polyCopy.length;
-			minValue = Double.MAX_VALUE;
-		  }
-		  return polyCopy;
-	  } else if (pLength == 4)
-		  return poly;
-	  else{
-    	  throw new IllegalArgumentException();
-	  }
-  }
-  
-  private static double[] removePoint(double[] array, int index){
-	  int length = array.length;
-	  double[] tmpArray = new double[length - 2];
-	  for(int m = 0; m <= index+1; m++){
+	public static double[] simplifyShape(double[] poly, int k)
+	{
+		double[] polyCopy = poly.clone();
+		int pLength = polyCopy.length;
+		double minValue = Double.MAX_VALUE;
+		int indexSValue = -1;
+		if(pLength>4){
+			while(pLength/2 > k){
+				for(int i = 0; i < pLength-6; i+=2){
+					double l1 = calcDistFromArray(polyCopy,i,i+2);
+					double l2 = calcDistFromArray(polyCopy,i+2,i+4);
+					double l3 = calcDistFromArray(polyCopy,i,i+4);
+
+					double value = (l1 + l2 - l3);
+					if(value < minValue){
+						minValue = value;
+						indexSValue = i+2;
+					}
+				}//end for
+				polyCopy = removePoint(polyCopy, indexSValue);
+				pLength = polyCopy.length;
+				minValue = Double.MAX_VALUE;
+			}//end while
+			return polyCopy;
+		} else if (pLength == 4)
+			return poly;
+		else{//should never happen?
+			throw new IllegalArgumentException();
+		}
+	}
+
+	private static double[] removePoint(double[] array, int index){
+		int length = array.length;
+		double[] tmpArray = new double[length - 2];
+		for(int m = 0; m < index; m++){
 			tmpArray[m] = array[m];
 		}
-		for(int j = index+4; j < length; j++){
-			tmpArray[index+2] = array[j];
+		for(int j = index+2; j < length; j++){
+			tmpArray[j-2] = array[j];
 		}
 		return tmpArray;
-  }
-  
-  private static double calcDist(double x, double y){
-	  return Math.sqrt(Math.pow(x,2)+ Math.pow(y,2));
-  }
-  
-  public static void main(String[] args){
-	  double[] test = {5,8,10,3,2,9,1,1,2,2,3,3};
-	  double[] out = simplifyShape(test, 4);
-	  System.out.print("{");
-	  for(int i = 0; i < out.length; i++){
-		  System.out.print(out[i] + ", ");
-	  }
-	  System.out.print("}");
-  }
+	}
+
+	private static double calcDistFromArray(double[] array, int point1, int point2){
+		double x = (array[point1]-array[point2]);
+		double y = (array[point1+1]-array[point2+1]);
+		return Math.sqrt(Math.pow(x,2) + Math.pow(y,2));
+
+	}
+
+	public static void main(String[] args){
+		double[] test = {
+				2.0145, 1.0134,
+				3.0746, 1.0232,
+				4.0163, 2.0972,
+				4.0516, 4.0152,
+				9.0862, 4.0917,
+				10.0828, 5.0998,
+				10.0862, 7.0552,
+				9.0382, 8.0277,
+				7.0287, 8.0735,
+				2.0933, 5.0293,
+				1.0238, 4.0023,
+				2.9283, 3.0283,
+				2.093, 2.0239
+		};
+		double[] out = simplifyShape(test, 8);
+		double[] out2 = simplifyShape(test, 6);
+		
+		System.out.print("{");
+		for(int i = 0; i < out.length; i++){
+			System.out.print(out[i] + ", ");
+		}
+		System.out.print("}");
+		System.out.println("");
+		System.out.println("");
+		System.out.print("{");
+		for(int i = 0; i < out2.length; i++){
+			System.out.print(out2[i] + ", ");
+		}
+		System.out.print("}");
+		
+		DrawGraph myGraph = new DrawGraph();
+		for(int i = 0; i < out2.length; i+=4){
+			//myGraph.drawLine(out[i], out[i+1], out[i+2], out[i+3], Color.RED, 5, 0);
+		}
+	}
 }
