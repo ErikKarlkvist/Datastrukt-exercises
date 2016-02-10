7657797d3856e1b340582c5b53387e000dabe20f
ErikKarlkvist <erikkarlkvist95@gmail.com>
2016-02-10 10:11:44 +0100

    very close

--- Excersises/src/partTwo/Lab2b.java
+++ Excersises/src/partTwo/Lab2b.java
@@ -18,20 +18,20 @@ public class Lab2b {
 			Node node1 = (Node)arg0;
 			Node node2 = (Node)arg1;
 	
-			if((node1.getNext() == null && node2.getPrev() == null) ||
+			/*if((node1.getNext() == null && node2.getPrev() == null) ||
 					((node2.getNext() == null && node1.getPrev() == null))){
 				System.out.println("first if");
-				return 0;
+				return 1;
 			}else if(node1.getNext() == null || node1.getPrev() == null){
-				System.out.println("second if");
 				return 1;
 			}else if(node2.getNext() == null  || node2.getPrev() == null){
-				System.out.println("third if");
 				return -1;
-			}else{
-				System.out.println("best if");
-				return (int)(calculateValue(node1) - calculateValue(node2));
-			}
+			}else{*/
+			double value1 = calculateValue(node1);
+			double value2 = calculateValue(node2);
+			int comp = Double.compare(value1,value2);
+			return (comp);
+			//}
 		}
 		
 		/**
@@ -59,31 +59,6 @@ public class Lab2b {
 		}
 		
 	}
-
-	/*public static class Point implements Comparable{
-		double x,y, value;
-		Point(double x, double y){
-			this.x = x;
-			this.y = y;
-		}
-		
-		public void setValue(double value){
-			this.value = value;
-		}
-		
-		public double getValue(){
-			return value;
-		}
-
-		@Override
-		public int compareTo(Object o) {
-			if(o instanceof Point){
-				Point compPoint = (Point)o;
-				return (int)(value - compPoint.getValue());	
-			}
-			throw new IllegalArgumentException();
-		}
-	};*/
 	
 	public static double[] simplifyShape(double[] poly, int k)
 	{
@@ -95,25 +70,36 @@ public class Lab2b {
 		first.setLocation(poly[0], poly[1]);
 		last.setLocation(poly[poly.length-2], poly[poly.length-1]);
 		pointList.addFirst(first);
-		pQueue.add(pointList.first);
+		//pQueue.add(pointList.first);
 		
 		pointList.addLast(last);
-		pQueue.add(pointList.last);
+		//pQueue.add(pointList.last);
 		for(int i=2; i < poly.length-2; i+=2){
 			Point p = new Point();
 			p.setLocation(poly[i], poly[i+1]);
 			pointList.insertBefore(p, pointList.last);
-			pQueue.add(pointList.last.prev);
+			//pQueue.add(pointList.last.prev);
 		}
 		
-		while(pQueue.size() > k){
+		DLList.Node node = pointList.getFirst().next;
+		while(node != pointList.getLast()){
+			pQueue.add(node);
+			node = node.next;
+		}
+		
+		while(pQueue.size()+2 > k){
 			Node rNode = pQueue.peek();
 			pointList.remove(rNode);
 			pQueue.remove(rNode);
-			pQueue.remove(rNode.next);
-			pQueue.remove(rNode.prev);
-			pQueue.add(rNode.next);
-			pQueue.add(rNode.prev);			
+			
+			if(rNode.prev != pointList.getFirst()){
+				pQueue.remove(rNode.prev);
+				pQueue.add(rNode.prev);
+			}
+			if(rNode.next != pointList.getLast()){
+				pQueue.remove(rNode.next);
+				pQueue.add(rNode.next);
+			}		
 		}
 		
 		Node theNode = pointList.getFirst();
@@ -124,38 +110,5 @@ public class Lab2b {
 			theNode = theNode.next;
 		}
 		return newArray;
-	}
-	
-	public static void main(String[] args){
-		double[] test = {
-				2.0145, 1.0134,
-				3.0746, 1.0232,
-				4.0163, 2.0972,
-				4.0516, 4.0152,
-				9.0862, 4.0917,
-				10.0828, 5.0998,
-				10.0862, 7.0552,
-				9.0382, 8.0277,
-				7.0287, 8.0735,
-				2.0933, 5.0293,
-				1.0238, 4.0023,
-				2.9283, 3.0283,
-				2.093, 2.0239
-		};
-		double[] out = simplifyShape(test, 8);
-		double[] out2 = simplifyShape(test, 6);
-		
-		System.out.print("{");
-		for(int i = 0; i < out.length; i++){
-			System.out.print(out[i] + ", ");
-		}
-		System.out.print("}");
-		System.out.println("");
-		System.out.println("");
-		System.out.print("{");
-		for(int i = 0; i < out2.length; i++){
-			System.out.print(out2[i] + ", ");
-		}
-		System.out.print("}");
 	}
 }
