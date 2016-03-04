4b108a020fac0f8855226f7c24f96fec7421c2cc
ErikKarlkvist <erikkarlkvist95@gmail.com>
2016-03-04 12:00:01 +0100

    kruskal done

--- Excersises/src/partFour/CompKruskalEdge.java
+++ Excersises/src/partFour/CompKruskalEdge.java
@@ -1,5 +1,19 @@
 package partFour;
 
-public class CompKruskalEdge {
+import java.util.Comparator;
 
+import partFour.DirectedGraph.Data;
+
+public class CompKruskalEdge <E extends Edge> implements Comparator<E>{
+
+	@Override
+	public int compare(E e1, E e2) {
+		if(e1 == null){
+			return -1;
+		}
+		if(e2 == null){
+			return 1;
+		}
+		return Double.compare(e1.getWeight(), e2.getWeight());
+	}
 }
--- Excersises/src/partFour/DirectedGraph.java
+++ Excersises/src/partFour/DirectedGraph.java
@@ -4,34 +4,34 @@ import java.util.*;
 
 
 public class DirectedGraph<E extends Edge> {
-	
+
 	public class Data{
 		private final int node; 
 		private final double distance;
-		
-		private final List<E> path;
-		public Data(int node, List<E> path, double distance){
+
+		private final LinkedList<E> path;
+		public Data(int node, LinkedList<E> path, double distance){
 			this.node = node;
 			this.distance = distance;
 			this.path = path;
 		}
-		
+
 		public int getNode() {
 			return node;
 		}
-	
+
 		public double getWeight() {
 			return distance;
 		}
-		
-		public List<E> getPath() {
+
+		public LinkedList<E> getPath() {
 			return path;
 		}
 	}
-	
+
 	private int nodes;
 	private List<E> edges;
-	
+
 	public DirectedGraph(int noOfNodes) {
 		nodes = noOfNodes;
 		edges = new LinkedList<E>();
@@ -44,7 +44,7 @@ public class DirectedGraph<E extends Edge> {
 	public Iterator<E> shortestPath(int from, int to) {
 		List<Integer> visitedNodes = new LinkedList<Integer>();
 		PriorityQueue<Data> pq = new PriorityQueue<Data>(new CompDijkstraPath<>());
-		List<E> path = new LinkedList<E>();
+		LinkedList<E> path = new LinkedList<E>();
 		pq.add(new Data(from, path, 0));
 		Data currData;
 		while(!pq.isEmpty()){
@@ -58,10 +58,10 @@ public class DirectedGraph<E extends Edge> {
 				return currData.getPath().iterator();
 			}
 			visitedNodes.add(currData.getNode());
-			
+
 			for(E e: edges){
 				if(e.from == currData.getNode() && !visitedNodes.contains(e.to)){
-					List<E> currPath = (List<E>)((LinkedList<E>) currData.getPath()).clone();//hmmmm
+					LinkedList<E> currPath = (LinkedList<E>) currData.getPath().clone();//hmmmm	 
 					currPath.add(e);
 					Data newData = new Data(e.to, currPath,currData.getWeight() +e.getWeight());
 					pq.add(newData);
@@ -70,10 +70,35 @@ public class DirectedGraph<E extends Edge> {
 		}
 		return null;
 	}
-		
-	public Iterator<E> minimumSpanningTree() {
-		return null;
-	}
 
+	public Iterator<E> minimumSpanningTree() {
+		Map<Integer, LinkedList<E>> paths = new HashMap<Integer, LinkedList<E>>();
+		PriorityQueue<E> pq = new PriorityQueue<E>(new CompKruskalEdge<E>());
+		E currEdge = null;
+		for(E e: edges){
+			paths.put(e.from, new LinkedList<E>());
+			paths.put(e.to, new LinkedList<E>());
+			pq.add(e);
+		}
+		while(!pq.isEmpty()){
+			while(!pq.isEmpty()){
+				int i = 0;
+				currEdge = pq.poll();
+				if(paths.get(currEdge.from)!= paths.get(currEdge.to)) break;
+				i++;
+			}
+			LinkedList<E> lk = paths.get(currEdge.from);
+			LinkedList<E> ll = paths.get(currEdge.to);
+			ll.add(currEdge);
+			ll.addAll(lk);
+			if(!ll.isEmpty()){
+				for(E e: ll){
+					paths.put(e.from, ll);
+					paths.put(e.to, ll);
+				}
+			} 
+		}
+		return paths.get(currEdge.from).iterator();
+	}
 }
-  
+
